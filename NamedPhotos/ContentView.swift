d389cf91950bce1a7419deacaac7dbc639bb0986
mlwelker <nutria.watts.0g@icloud.com>
2023-09-20 19:52:47 -0500

    Initial Commit

---
+++ NamedPhotos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NamedPhotos
+//
+//  Created by Michael Welker on 2023-09-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NamedPhotos/NamedPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamedPhotosApp.swift
+//  NamedPhotos
+//
+//  Created by Michael Welker on 2023-09-20.
+//
+
+import SwiftUI
+
+@main
+struct NamedPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
